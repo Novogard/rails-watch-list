7169870f495244c57dcf238d3d9819cc3039bcd4
Jonas Capelier <capelier.jonas@gmail.com>
2025-05-22 17:56:33 +0200

    Style to do

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,35 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "Cleaning DB"
+Faker::UniqueGenerator.clear
+Movie.destroy_all
+List.destroy_all
+Bookmark.destroy_all
+
+puts "DB cleaned"
+
+puts "Building DB"
+10.times do
+  Movie.create!(title: Faker::Movie.unique.title,
+                overview: Faker::Movie.unique.quote,
+                poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg",
+                rating: rand(0.0..10.0).round(1))
+  end
+puts "Building lists DB"
+  10.times do
+    List.create!(name: Faker::Fantasy::Tolkien.unique.poem)
+  end
+puts "Lists DB built"
+
+puts "Building bookmarks DB"
+movie_id_counter = 0
+list_id_counter = 0
+  10.times do
+    Bookmark.create!(comment: Faker::Quotes::Shakespeare.unique.romeo_and_juliet_quote,
+                     movie: Movie.find(rand(1..10)),
+                     list: List.find(rand(1..10)))
+    movie_id_counter += 1
+    list_id_counter += 1
+  end
+puts "Bookmarks DB created"
