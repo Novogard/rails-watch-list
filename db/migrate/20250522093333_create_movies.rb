250cac15324e987dda0ebc6ab1f25040ff58a284
Jonas Capelier <capelier.jonas@gmail.com>
2025-05-22 11:34:21 +0200

    rails new

---
+++ db/migrate/20250522093333_create_movies.rb
@@ -0,0 +1,11 @@
+class CreateMovies < ActiveRecord::Migration[7.1]
+  def change
+    create_table :movies do |t|
+      t.string :overview
+      t.string :poster_url
+      t.decimal :rating
+
+      t.timestamps
+    end
+  end
+end
