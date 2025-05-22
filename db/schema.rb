9ae388deedfa5903541b9901fb37142574711528
Jonas Capelier <capelier.jonas@gmail.com>
2025-05-22 11:55:33 +0200

    Models and setup done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_22_093408) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_22_094838) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,6 +36,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_22_093408) do
     t.decimal "rating", precision: 3, scale: 1
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
   end
 
   add_foreign_key "bookmarks", "lists"
